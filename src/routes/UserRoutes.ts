6b266937e4f4da929169516cd7acacfbf379cf97
Igor Giuliano <igor.andrade07@gmail.com>
2023-04-08 16:43:39 -0300

    Updated User Routes

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,10 +1,16 @@
+import { Router } from 'express';
+import { DisableUserController } from '@controllers/users/DisableUserController';
+import { EditUserController } from '@controllers/users/EditUserController';
+import { ListUsersController } from '@controllers/users/ListUsersController';
 import { LoginController } from '@controllers/users/LoginController';
 import { RegisterUserController } from '@controllers/users/RegisterUserController';
-import { Router } from 'express';
 
 const userRoutes = Router();
 
 userRoutes.post('/login', new LoginController().handle);
 userRoutes.post('/register_user', new RegisterUserController().handle);
+userRoutes.put('/edit_user', new EditUserController().handle);
+userRoutes.get('/list_users', new ListUsersController().handle);
+userRoutes.delete('/delete_user', new DisableUserController().handle);
 
 export { userRoutes };
