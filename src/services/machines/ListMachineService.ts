94766bbd13710dd5cb438c5186639e13d01844c7
Igor Giuliano <igor.andrade07@gmail.com>
2023-04-13 11:29:58 -0300

    Changes

--- src/services/machines/ListMachineService.ts
+++ src/services/machines/ListMachineService.ts
@@ -3,10 +3,7 @@ import { prisma } from '../../database/database';
 class ListMachinesService {
     async execute() {
         const machine = await prisma.machine.findMany({
-            select: {
-                id_machine: true,
-                location: true
-            }
+            
         });
 
         return machine;
